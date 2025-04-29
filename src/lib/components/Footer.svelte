5d384b6c0d0438b09167e31e6d5631504e60b668
Alessandro Bonanno <alessandro.bonanno@edreamsodigeo.com>
2025-04-29 07:50:02 +0200

    feat: improve footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+  import logo from "$lib/assets/svelte.svg";
+  import {version} from "../../../package.json";
+</script>
+
 <footer>
-	<p>Made with love by Alessandro Bonanno - Uncharted POD - Travel Things Area</p>
+  <b>DataWeaver {version}</b> - Made with <span>❤️</span> & <img src={logo} alt="svelte logo"> by Alessandro Bonanno - Uncharted Pod - Travel Things Area 
 </footer>
+
+<style>
+  span, img {
+    display: inline-block;
+  }
+  
+  span, img {    
+    margin-inline: .2rem;
+  }
+  
+  b {    
+    margin-inline-end: .2rem;
+  }
+  
+  img {
+    width: 16px;
+    height: 16px;
+    vertical-align: text-bottom;
+  }
+</style>
